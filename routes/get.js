51f1adc8a528a10f9a641741b0cf52353d88fda0
heretics7 <heretics7@naver.com>
2022-03-18 16:53:39 +0900

    AWS_SQL 연결 세팅 성공

--- routes/get.js
+++ routes/get.js
@@ -1,8 +1,19 @@
 var express = require('express');
-var getRouter = express.Router();
+var getrouter = express.Router();
+var mysql = require('mysql');
+var awsconfig = require('../db/awsconfig.js')
+var connect = mysql.createConnection(awsconfig);
 
-getRouter.get('/', (req, res) => {
-    res.send('localhost:8080/get 연결 성공')
+getrouter.get('/', (req, res) => {
+    res.send('localhost:8080/get 라우터 연결 성공')
+})
+getrouter.get('/suppose', (req, res) => {
+    connect.query('SELECT * FROM suppose', (error, data) => {
+        if(error) throw error;
+            console.log('react_suppose 스키마 suppose 테이블 내 데이터', data)
+            res.send(data)
+        }
+    )
 })
 
-module.exports = getRouter;
+module.exports = getrouter;
