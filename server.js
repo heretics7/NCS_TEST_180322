61b832f6edb478df553b04a493e003891f9afa86
--global <--global>
2022-03-19 10:41:22 +0900

    post 연결 설정

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-var getRouter = require('./routes/get')
+var getrouter = require('./routes/get');
+var postrouter = require('./routes/post');
 
 
 app.set('port', 8080)
@@ -8,7 +9,8 @@ app.get('/', (req, res) => {
     res.send('localhost:8080 연결 성공')
 })
 
-app.use('/get', getRouter)
+app.use('/get', getrouter)
+app.use('/post', postrouter)
 
 app.listen(app.get('port'), () => {
     console.log('실행되면 노드 연결 성공')
